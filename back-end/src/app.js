a7367d53c7584768d043e17b443e038384da0798
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-21 14:43:08 -0600

    added files for table

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -9,6 +9,7 @@ const errorHandler = require('./errors/errorHandler');
 const notFound = require('./errors/notFound');
 
 const reservationsRouter = require('./reservations/reservations.router');
+const tablesRouter = require('./tables/tables.router');
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/reservations', reservationsRouter);
+app.use('/tables', tablesRouter);
 
 app.use(notFound);
 app.use(errorHandler);
