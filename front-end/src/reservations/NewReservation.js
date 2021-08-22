4509caf9366c96026a0d8e57741d97c9d6912447
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-22 14:25:32 -0600

    added new reservation file

---
+++ front-end/src/reservations/NewReservation.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Form from "./Form"
+
+export default function NewReservation() {
+ 
+
+  return (
+    <div>
+      <h4 className='text-center pt-5'>Make a New Reservation</h4>
+      <Form />
+    </div>
+  );
+}
