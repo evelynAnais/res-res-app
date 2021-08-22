5ca41b39245350ca292937b2cbdae7c0206e2b69
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-22 15:33:45 -0600

    added card for reservations

--- front-end/src/reservations/ReservationCard.js
+++ front-end/src/reservations/ReservationCard.js
@@ -1,7 +1,17 @@
-export default function ReservationCard() {
+export default function ReservationCard({ reservation }) {
   return (
     <>
-      CARD
+      <div className="card" style={{'width': '18rem'}}>
+        <div className="card-header">
+          {reservation?.first_name} {reservation?.last_name}
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Mobile Number: {reservation?.mobile_number}</li>
+          <li className="list-group-item">Reservation Date: {reservation?.reservation_date}</li>
+          <li className="list-group-item">Reservation Time: {reservation?.reservation_time}</li>
+          <li className="list-group-item">People: {reservation?.people}</li>
+        </ul>
+      </div>
     </>
   )
 }
