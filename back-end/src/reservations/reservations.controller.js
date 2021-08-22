cc8688e2cedf19f57b4048a501d78dd4ec2a4d1f
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-21 22:00:35 -0600

    deleted unecesary code

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -4,13 +4,6 @@ const hasProperties = require("../errors/hasProperties");
 const hasRequiredProperties = hasProperties('first_name', 'last_name', 'mobile_number', 'reservation_date', 'reservation_time', 'people');
 
 const VALID_PROPERTIES = [
-  // 'first_name',
-  // 'last_name',
-  // 'mobile_number',
-  // 'reservation_date',
-  // 'reservation_time',
-  // 'people',
-  // 'status',
   'first_name',
   'last_name',
   'mobile_number',
@@ -36,22 +29,6 @@ function hasOnlyValidProperties(req, res, next) {
   }
   next();
 }
-
-// function hasOnlyValidProperties(req, res, next) {
-//   const { data = {} } = req.body;
-
-//   const invalidFields = Object.keys(data).filter(
-//     (field) => !VALID_PROPERTIES.includes(field)
-//   );
-
-//   if (invalidFields.length) {
-//     return next({
-//       status: 400,
-//       message: `Invalid field(s): ${invalidFields.join(", ")}`,
-//     });
-//   }
-//   next();
-// }
 
 async function reservationExists(req, res, next) {
   const reservation = await service.read(req.params.reservationId);
@@ -129,17 +106,3 @@ module.exports = {
   update: [asyncErrorBoundary(reservationExists), hasOnlyValidProperties, hasRequiredProperties, asyncErrorBoundary(update)],
   delete: [asyncErrorBoundary(reservationExists), asyncErrorBoundary(destroy)]
 }
-
-
-/**
- * List handler for reservation resources
- */
-// async function list(req, res) {
-//   res.json({
-//     data: [],
-//   });
-// }
-
-// module.exports = {
-//   list,
-// };
