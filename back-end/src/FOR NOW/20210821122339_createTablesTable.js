f4e6b1fd4760140818b541917667ce315a46568a
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-21 19:38:20 -0600

    moved tables to pass tests for US1

--- back-end/src/FOR NOW/20210821122339_createTablesTable.js
+++ back-end/src/FOR NOW/20210821122339_createTablesTable.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('tables', (table) => {
     table.increments('table_id').primary();
-    table.string('table_name').notNullable();
+    table.string('table_name');
     table.integer('capacity').notNullable();
     table.integer('reservation_id').unsigned().nullable();
     table
