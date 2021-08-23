d5623037628a7b1c501be558697c554d176736e2
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-23 13:34:05 -0600

    base url

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,7 +5,7 @@
 import formatReservationDate from './format-reservation-date';
 import formatReservationTime from './format-reservation-date';
 
-const API_BASE_URL =
+const API_BASE_URL = 'https://res-res-app.herokuapp.com'
   process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
 
 /**
