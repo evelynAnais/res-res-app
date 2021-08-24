6aba39911bb60cdf556fd9670cd2be919b90a4aa
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-23 20:50:22 -0600

    set up table form

--- front-end/src/tables/NewTable.js
+++ front-end/src/tables/NewTable.js
@@ -1,7 +1,12 @@
+import TableForm from "./TableForm";
+
 export default function NewTable() {
   return (
     <>
-      New Table
+      <div>
+        <h4 className='text-center pt-5'>Make a New Table</h4>
+        <TableForm />
+      </div>
     </>
   )
 }
--- front-end/src/tables/TableForm.js
+++ front-end/src/tables/TableForm.js
@@ -1,7 +1,83 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router';
+import ErrorAlert from "../layout/ErrorAlert";
+
 export default function TableForm() {
+  const [newTable, setNewTable] = useState({
+    table_name: '',
+    capacity: '1',
+  })
+
+  const [error, setError] = useState(null);
+
+  const history = useHistory()
+  
+  // function handleChange({ target }) {
+  //   let newValue = target.value;
+  //   if (target.name === 'people') {
+  //     newValue = Number(target.value);
+  //   }
+  //   setNewReservation((previousTable) => ({
+  //     ...previousTable,
+  //     [target.name]: newValue,
+  //   }));
+  // }
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   createReservation(newTable)
+  //     .then(() => history.push(`/dashboard?date=${formatAsDate(newReservation.reservation_date)}`))
+  //     .catch(setError);
+  // }
+
+  const handleCancel = () => {
+    history.push('/')
+  }
+
   return(
     <>
-      Table Form
+      <div className='container'>
+      <form className='row d-flex'>
+        <label htmlFor='table_name' className='form-label'>
+          Table Name:
+          <input 
+            className='form-control mb-3'
+            type='text' 
+            id='table_name' 
+            name='table_name'
+            //value={newTable.table_name}
+            //onChange={handleChange}
+            required={true}
+          />
+        </label>
+        <label htmlFor='capacity'>
+          Capacity:
+          <input 
+            className='form-control mb-3'
+            type='number' 
+            id='capacity' 
+            name='capacity'
+            min='1'
+            placeholder='1'
+            //value={newTable.capacity}
+            //onChange={handleChange}
+            required={true}
+          />
+        </label>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-xs-12'>
+            <div  className='text-center'>
+              <ErrorAlert error={error} />
+              <button type='submit' className='btn btn-dark mr-3 ml-3'>Submit</button>
+              <button type='button' onClick={handleCancel} className='btn btn-dark mr-3 ml-3'>Cancel</button>
+              {/* <button type='submit' onClick={handleSubmit} className='btn btn-dark mr-3 ml-3'>Submit</button> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+    </div>
     </>
   )
 }
