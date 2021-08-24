5862ffd56fdd446417845f0d36ee05363b2944ff
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-23 22:31:42 -0600

    formatting and styling

--- front-end/src/reservations/ReservationCard.js
+++ front-end/src/reservations/ReservationCard.js
@@ -1,8 +1,8 @@
 export default function ReservationCard({ reservation }) {
   return (
     <>
-      <div className='card mt-3' style={{'width': '18rem'}}>
-        <div className='card-header'>
+      <div className='card border-dark text-white bg-dark mt-3' style={{'width': '18rem'}}>
+        <div className='card-header bg-transparent'>
           {reservation?.first_name} {reservation?.last_name}
         </div>
         <ul className='list-group list-group-flush'>
@@ -10,6 +10,10 @@ export default function ReservationCard({ reservation }) {
           <li className='list-group-item'>Reservation Date: {reservation?.reservation_date}</li>
           <li className='list-group-item'>Reservation Time: {reservation?.reservation_time}</li>
           <li className='list-group-item'>People: {reservation?.people}</li>
+          <div class="card-footer bg-transparent border-dark">
+            <a href={`/reservations/${reservation?.reservation_id}/seat`} class="btn btn-secondary ml-5">Seat</a>
+          </div>
+          
         </ul>
       </div>
     </>
--- front-end/src/reservations/ReservationList.js
+++ front-end/src/reservations/ReservationList.js
@@ -3,7 +3,7 @@ import ReservationCard from './ReservationCard';
 export default function ReservationList({ reservations }) {
   const resList = reservations.map((reservation, reservation_id) => (
     <ReservationCard key={reservation_id} reservation={reservation} />
-  ))
+  ));
   return(
     <>
       {resList.length ? resList : null} 
