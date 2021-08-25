ef0baf374fa5a817b63392e0074159aa2f6cf2f8
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-24 22:59:48 -0600

    added button to table card

--- front-end/src/tables/TableCard.js
+++ front-end/src/tables/TableCard.js
@@ -1,11 +1,15 @@
 export default function TableCard({ table }) {
+  
   return(
     <>
       <div className='card border-secondary mb-3' style={{'width': '18rem'}}>
         <div className='card-header bg-transparent'>Table Name: {table?.table_name}</div>
-        <div className='card-body text-dark'>
+        <div className='card-body text-dark pb-0'>
           <h5 className='card-title' data-table-id-status={`${table.table_id}`}>{table.reservation_id ? 'Occupied' : 'Free'}</h5>
           <p className='card-text' >Capacity: {table.capacity}</p>
+          <div className='card-footer bg-transparent border-dark'>
+            <button type='button' data-table-id-finish={table.table_id} className='btn btn-secondary btn-block'>Finish</button>
+          </div>
         </div>
       </div>
     </>
