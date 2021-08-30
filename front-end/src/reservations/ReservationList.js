a342ebb20ecde29bb8c15bcfd25c4e9ac6a67103
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-30 17:55:02 -0600

    conditionally rendered list component

--- front-end/src/reservations/ReservationList.js
+++ front-end/src/reservations/ReservationList.js
@@ -1,13 +1,21 @@
 import ReservationCard from './ReservationCard';
+import { useRouteMatch } from "react-router-dom";
+
 
 export default function ReservationList({ reservations }) {
+  const { path } = useRouteMatch();
+
   const resList = reservations.map((reservation, reservation_id) => (
     <ReservationCard key={reservation_id} reservation={reservation} />
   ));
+
   return(
     <div className='card-group'>
       {resList.length ? resList : null} 
-      {!resList.length && <p>No reservations this date</p>}
+      {path.includes('search') ? 
+      !resList.length && <p>No reservations found</p>
+      :
+      !resList.length && <p>No reservations this date</p>}
     </div>
   )
 }
