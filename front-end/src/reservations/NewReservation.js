b7855088e7c63104213bf5232bbfe8fe9e83d67b
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-30 20:31:39 -0600

    added cancel status

--- front-end/src/reservations/NewReservation.js
+++ front-end/src/reservations/NewReservation.js
@@ -1,10 +1,15 @@
 import ReservationForm from './ReservationForm'
+import { useRouteMatch } from "react-router-dom";
 
 export default function NewReservation() {
+  const { path } = useRouteMatch();
+
   return (
     <div>
-      <h4 className='text-center pt-5'>Make a New Reservation</h4>
-      <ReservationForm />
+      {path.includes('edit') 
+        ? <h4 className='text-center pt-5'>Edit Reservation</h4>
+        : <h4 className='text-center pt-5'>Make a New Reservation</h4>}
+        <ReservationForm />
     </div>
   );
 }
--- front-end/src/reservations/ReservationCard.js
+++ front-end/src/reservations/ReservationCard.js
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import { useRouteMatch } from "react-router-dom";
+import ErrorAlert from "../layout/ErrorAlert";
+import { updateReservationStatus } from "../utils/api";
 
-export default function ReservationCard({ reservation }) {
+export default function ReservationCard({ reservation, loadDashboard }) {
+  const [cancelError, setCancelError] = useState(null);
   const { path } = useRouteMatch();
+  
+  const handleCancel = (e) => {
+    e.preventDefault();
+    if (window.confirm('Do you want to cancel this reservation? This cannot be undone.')) {
+      const abortController = new AbortController ();
+      setCancelError(null);
+      const update = {
+        reservation_id: e.target.value,
+        status: 'cancelled'
+      }
+      updateReservationStatus(update, abortController.signal)
+        .then((res) => console.log(res))
+        .then(loadDashboard())
+        .catch(setCancelError);
+      return () => abortController.abort()
+    }
+  };
   
   return (
     <>
-      <div className='card border-secondary text-white bg-dark mt-3' style={{'width': '18rem'}}>
+      {reservation.status !== 'cancelled' && <div className='card border-secondary text-white bg-dark mt-3' style={{'width': '18rem'}}>
         <div className='card-header bg-transparent'>
           {reservation?.first_name} {reservation?.last_name}
         </div>
@@ -20,10 +41,22 @@ export default function ReservationCard({ reservation }) {
               ? null 
               : reservation?.status === 'seated' 
               ? null 
-              : <a href={`/reservations/${reservation?.reservation_id}/seat`} className='btn btn-secondary btn-block'>Seat</a>}
+              : <>
+                  <a href={`/reservations/${reservation?.reservation_id}/seat`} className='btn btn-secondary btn-block'>Seat</a>
+                  <a href={`/reservations/${reservation?.reservation_id}/edit`} className='btn btn-secondary btn-block'>Edit</a>
+                  <button 
+                    type='button' 
+                    onClick={handleCancel} 
+                    value={reservation?.reservation_id} 
+                    data-reservation-id-cancel={reservation?.reservation_id}
+                    className='btn btn-secondary btn-block'
+                    >Cancel
+                  </button>
+                  <ErrorAlert error={cancelError} />
+                </>}
           </div>
         </ul>
-      </div>
+      </div>}
     </>
   )
 }
